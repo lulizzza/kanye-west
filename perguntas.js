ea98374b410d565defd03adeda43eea8a57b72f6
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-10-09 08:48:05 -0300

    wedfgh

---
+++ perguntas.js
@@ -0,0 +1,123 @@
+criaCartao(
+    "matematica",
+    "como se calcula a soma dos termos de uma P.A?",
+    "utiliza-se a formula; ((a1+an)*r)/2"
+    
+)
+
+criaCartao(
+    "filosofia",
+    "De quem é a famosa frase “Penso, logo existo”?",
+    "Descartes"
+    
+)
+criaCartao(
+    "geografia",
+    "quantos paises tem no mundo?",
+    "193 países"
+    
+)
+criaCartao(
+    "quimica",
+    "qual elemento de numero 29 na tabela periodica?",
+    "cobre(CU)"
+    
+)
+criaCartao(
+    "biologia",
+    "o que é um orgão homologo?",
+    "são aqueles que podem ou não realizar a mesma função, porém apresentam uma estrutura básica igual e mesmo desenvolvimento embrionário"
+    
+)
+criaCartao(
+    "historia",
+    "quem foi o primeiro presidente do Brasil?",
+    "Marechal Deodoro da Fonseca"
+    
+)
+criaCartao(
+    "matematica",
+    "formula para calcular uma circunferencia?",
+    "C = 2πr"
+    
+)
+criaCartao(
+    "geografia",
+    "o que é um relevo?",
+    " definido como sendo as formas da superfície de um planeta"
+    
+)
+criaCartao(
+    "quimica",
+    "do que é composto um hidrocarboneto?",
+    "somente carbono(C) e hidrogenio(H)"
+    
+)
+criaCartao(
+    "biologia",
+    "o que sao alelos?",
+    "Os alelos são as formas alternativas, gerados por mutações, de um mesmo gene."
+    
+)
+criaCartao(
+    "portugues",
+    "o que é um verbo?",
+    "palavra que determina ação"
+    
+)
+criaCartao(
+    "ingles",
+    "como é cachorro em ingles?",
+    "dog"
+    
+)
+criaCartao(
+    "geografia",
+    "quantos oceanos existem no mundo?",
+    "5"
+    
+)
+criaCartao(
+    "matematica",
+    "quanto é 1+1?",
+    "2"
+    
+)
+criaCartao(
+    "historia",
+    "quantas constituiçoes brasileiras existem?",
+    "8"
+    
+)
+criaCartao(
+    "portugues",
+    "o que é um sinonimo?",
+    "Sinônimos são palavras que possuem o mesmo ou aproximadamente o mesmo significado"
+    
+)
+criaCartao(
+    "matematica",
+    "como se calcula a soma dos termos de uma P.A?",
+    "utiliza-se a formula; ((a1+an)*r)/2"
+    
+)
+criaCartao(
+    "matematica",
+    "como se calcula a soma dos termos de uma P.A?",
+    "utiliza-se a formula; ((a1+an)*r)/2"
+    
+)
+criaCartao(
+    "matematica",
+    "como se calcula a soma dos termos de uma P.A?",
+    "utiliza-se a formula; ((a1+an)*r)/2"
+    
+)
+criaCartao(
+    "matematica",
+    "como se calcula a soma dos termos de uma P.A?",
+    "utiliza-se a formula; ((a1+an)*r)/2"
+    
+)
+ // ao todo são 20 perguntas que exijam memorização dos conteudos 
+ // do seu ensino médio (qualquer disciplina)
