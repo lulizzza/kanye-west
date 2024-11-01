58b0df85866f18975545be328fee49a6bade585a
lulizzza <142451759+lulizzza@users.noreply.github.com>
2024-11-01 08:25:17 -0300

    rtghj

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categotia, pergunta, resposta){
     let cartao = document.createElement("article");
     cartao.className = "cartao";
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick = "mostrarResposta(this)">
     <h3>${categotia}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -18,4 +18,11 @@ function criaCartao (categotia, pergunta, resposta){
 
 function mostrarResposta(card){
     const resposta =card.querySelector(".resposta-cartao");
+
+    if(resposta.style.display === "none"){
+       resposta.style.display = "block";
+    }
+    else{
+       resposta.style.display = "none";
+    }
 }
